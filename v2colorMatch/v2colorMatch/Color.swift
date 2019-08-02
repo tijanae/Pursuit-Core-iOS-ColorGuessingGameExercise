bb1d2ca5d3db133be66ee8bac40e1a97e5f9306b
Tia Lendor <TiaLendon@pursuit.org>
2019-08-02 18:08:32 -0400

    create ui

---
+++ v2colorMatch/v2colorMatch/Color.swift
@@ -0,0 +1,17 @@
+//
+//  Color.swift
+//  v2colorMatch
+//
+//  Created by Tia Lendor on 8/2/19.
+//  Copyright © 2019 Tia Lendor. All rights reserved.
+//
+
+import UIKit
+
+
+struct Color {
+    
+    static func getRandomColor () -> UIColor{
+        
+    }
+}
--- v2colorMatch/v2colorMatch/ViewController.swift
+++ v2colorMatch/v2colorMatch/ViewController.swift
@@ -10,11 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var colorView: UIView!
+    
+    @IBOutlet weak var urScoreLabel: UILabel!
+    @IBOutlet weak var hiScoreLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func colorButtonPressed(_ sender: UIButton) {
+        switch sender.tag {
+        case 0:
+            print("red button")
+        case 1:
+            print("green ")
+        case 2:
+            print("blue")
+        default:
+            fatalError()
+        }
+    }
+    
 }
 
